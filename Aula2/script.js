4954c713fc933c6c6ec72e767ee3701afc48d51e
Jonathan S. Hayasshi <jowhayasshi@gmail.com>
2025-05-09 16:57:15 -0300

    aula 3 desafio concluido

--- Aula2/script.js
+++ Aula2/script.js
@@ -2,14 +2,17 @@ function adicionarTarefa() {
     let mensagem = "Tarefa adicionada com sucesso!";
     let erro = "Tarefa não encontrada"
 
-    let inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
+    const inputTarefa = document.getElementById("inputTarefa")
+    const pegarMensagem = document.getElementById("mensagem")
+    let tarefa = inputTarefa.value.trim()
 
     if (tarefa == ""){
-        document.getElementById("mensagem").textContent = erro
+        pegarMensagem.textContent= erro
+        pegarMensagem.style.color = "#a34743"
     }else{
-        document.getElementById("mensagem").textContent = mensagem;
-        let listaTarefa = document.getElementById("listaTarefas")
+        pegarMensagem.textContent = mensagem;
+        pegarMensagem.style.color = "#28a745"
+        const listaTarefa = document.getElementById("listaTarefas")
         let novaTarefa = document.createElement("li")
     
         novaTarefa.textContent = tarefa
